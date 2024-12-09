63e41018e5a723c5c74dd8cfec33206eb5b0c8e3
Alejandro Moises Vera Flores <alvera-f@c2r5s5.42madrid.com>
2024-12-09 16:50:32 +0100

    entrega

--- level4/rev_wstr/rev_wstr.c
+++ level4/rev_wstr/rev_wstr.c
@@ -2,33 +2,35 @@
 
 int main (int argc, char *argv[])
 {
-	int i = 0;
-	int start;
-	int end;
-
 	if (argc == 2)
 	{
-		while (argv[1][i])
+		int i = 0;
+		int start;
+		int end;
+
+		while(argv[1][i])
 			i++;
-		while (i >= 0)
+		i--;
+		while (i > 0)
 		{
-			while( argv[1][i] == '\0' || argv[1][i] == ' ' || argv[1][i] == '\t')
+			while (i >= 0 && (argv[1][i] == ' ' || argv[1][i] == '\t'))
 				i--;
 			end = i;
-			while(argv[1][i] && argv[1][i] != ' ' && argv[1][i] != '\t')
+			while((argv[1][i] != ' ' && argv[1][i] != '\t') && i >= 0)
 				i--;
 			start = i + 1;
-			int flag;
-			flag = start;
 			while (start <= end)
 			{
 				write(1, &argv[1][start], 1);
 				start++;
 			}
-			if (flag != 0)
+			if (i > 0)
 				write(1, " ", 1);
-		}
+			else
+				break;
 
+		}
 	}
 	write (1, "\n", 1);
+	return (0);
 }
